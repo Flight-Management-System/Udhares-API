6dbe98f543291957f91db539706684cdf1e88f38
Yaseen <ahm3d.yaseen@gmail.com>
2024-03-10 23:09:41 +0500

    feat: passenger model with default controller

---
+++ app/Models/Passenger.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Passenger extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'passport_no',
+    ];
+}
